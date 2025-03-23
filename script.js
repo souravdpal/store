c1cffbb695047286d837a33adb589e974cc55ea8
sourav <sourav2026resolution@gmail.com>
2025-03-23 08:45:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
-
-    menuToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
 // Open the modal and set the image source
 function openModal(imageSrc) {
     const modal = document.getElementById('imageModal');
